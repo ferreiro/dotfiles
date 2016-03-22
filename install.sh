b2531edab0102907bc73c177c1f46a257cec501e
Jorge Ferreiro <me@jgferreiro.com>
2016-03-22 23:25:02 +0100

    Renamed brew-cask and brew-cask-fonts to cask

--- install.sh
+++ install.sh
@@ -14,5 +14,5 @@ ln -sfv "$DOTFILES_DIR/config/git/.gitignore_global" ~
 
 # Install Package managers and programs
 . "$DOTFILES_DIR/install/brew/brew.sh"
-. "$DOTFILES_DIR/install/brew/brew-cask.sh"
-. "$DOTFILES_DIR/install/brew/brew-cask-fonts.sh"
+. "$DOTFILES_DIR/install/brew/cask.sh"
+. "$DOTFILES_DIR/install/brew/cask-fonts.sh"
