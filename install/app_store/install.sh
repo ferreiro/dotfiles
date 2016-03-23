d31c68ac523bc2489487e0ce17334df6bffaab48
Jorge Ferreiro <me@jgferreiro.com>
2016-03-23 04:44:12 +0100

    Add appstore apps

---
+++ install/app_store/install.sh
@@ -0,0 +1,15 @@
+######################################
+### Install APP Store applications ###
+######################################
+
+echo ""
+echo "**************************************"
+echo "*** Install App Store Applications ***"
+echo "**************************************"
+echo ""
+
+
+## get from App Store
+#brew cask install --appdir="/Applications" evernote
+#brew cask install --appdir="/Applications" wunderlist
+#brew cask install --appdir="/Applications" clamxav
