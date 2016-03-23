6ec69d93be3b07d044b12129e0faa2b648966e9e
Jorge Ferreiro <me@jgferreiro.com>
2016-03-23 04:36:31 +0100

    Added Preferred icons to dock

--- osx/dock.sh
+++ osx/dock.sh
@@ -1,27 +1,23 @@
 #!/bin/sh
 # Source: https://github.com/webpro/dotfiles/blob/c55045c692088105dce3545637d8ab80f120908f/osx/dock.sh
 
-# dockutil --no-restart --remove all
-# dockutil --no-restart --add "$HOME/Applications/Wunderlist.app"
-# dockutil --no-restart --add "/Applications/Calendar.app"
-# dockutil --no-restart --add "/Applications/Airmail 2.app"
-# dockutil --no-restart --add "$HOME/Applications/Google Chrome.app"
-# dockutil --no-restart --add "$HOME/Applications/FirefoxDeveloperEdition.app"
-# dockutil --no-restart --add "$HOME/Applications/Atom.app"
-# dockutil --no-restart --add "$HOME/Applications/Iterm2.app"
-# dockutil --no-restart --add "/Applications/System Preferences.app"
-#
-# killall Dock
-
 dockutil --no-restart --remove all
-dockutil --no-restart --add "$HOME/Applications/Google Chrome.app"
-dockutil --no-restart --add "$HOME/Applications/Firefox.app"
-dockutil --no-restart --add "/Applications/Safari.app"
-dockutil --no-restart --add "/Applications/Mail.app"
+dockutil --no-restart --add "$HOME/Applications/Wunderlist.app"
 dockutil --no-restart --add "/Applications/Calendar.app"
-dockutil --no-restart --add "/Applications/Utilities/Terminal.app"
-dockutil --no-restart --add "/Applications/System Preferences.app"
-dockutil --no-restart --add "/Applications/Utilities/Console.app"
-dockutil --no-restart --add "$HOME/Applications/Spotify.app"
+dockutil --no-restart --add "/Applications/Airmail 2.app"
+dockutil --no-restart --add "$HOME/Applications/Google Chrome.app"
+dockutil --no-restart --add "$HOME/Applications/FirefoxDeveloperEdition.app"
+dockutil --no-restart --add "$HOME/Applications/Atom.app"
+dockutil --no-restart --add "$HOME/Applications/iterm2/iTerm.app"
 
-killall Dock
+# Add space to System configuration
+# defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="spacer-tile";}'
+
+#Add a spacer to the left side of the Dock (where the applications are)
+defaults write com.apple.dock persistent-apps -array-add '{tile-data={}; tile-type="spacer-tile";}'
+#Add a spacer to the right side of the Dock (where the Trash is)
+defaults write com.apple.dock persistent-others -array-add '{tile-data={}; tile-type="spacer-tile";}'
+
+
+dockutil --no-restart --add "/Applications/Safari.app"
+dockutil --no-restart --add "/Applications/System Preferences.app"
