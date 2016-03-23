7d03e42d552d8c79aaefe29a3337956bb870d93c
Jorge Ferreiro <me@jgferreiro.com>
2016-03-23 04:35:48 +0100

    Added defaults dock

---
+++ osx/defaults/defaults_dock.sh
@@ -0,0 +1,116 @@
+###############################################################################
+# Dock                                                                        #
+###############################################################################
+
+defaults delete com.apple.dock
+
+# Dock in the left of the screen
+defaults write com.apple.dock orientation -string "left"
+defaults write com.apple.dock pinning -string "end"
+
+defaults write com.apple.dock tilesize -int 27 # icon size
+defaults write com.apple.dock size-immutable -bool YES
+
+
+########################
+### SHOW / HIDE DOCK ###
+########################
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+# Change or Disable Auto-Hide Delay
+defaults write com.apple.dock autohide-time-modifier -float 0.8;
+
+# Remove the auto-hiding Dock delay
+defaults write com.apple.dock autohide-delay -float 0
+
+########################
+
+# Show indicator lights for open applications in the Dock
+defaults write com.apple.dock show-process-indicators -bool true
+
+# Enable highlight hover effect for the grid view of a stack (Dock)
+defaults write com.apple.dock mouse-over-hilite-stack -bool true
+
+# Don’t animate opening applications from the Dock
+defaults write com.apple.dock launchanim -bool false
+
+# Minimize windows into their application’s icon
+defaults write com.apple.dock minimize-to-application -bool true
+
+# Show only open applications in the Dock
+#defaults write com.apple.dock static-only -bool true
+
+# Make Dock icons of hidden applications translucent
+defaults write com.apple.dock showhidden -bool true
+
+# Don’t automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
+# # Change minimize/maximize window effect
+# defaults write com.apple.dock mineffect -string "scale"
+
+# # Enable spring loading for all Dock items
+# defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
+
+
+
+
+
+
+
+
+#
+# # Speed up Mission Control animations
+# defaults write com.apple.dock expose-animation-duration -float 0.1
+#
+# # Don’t group windows by application in Mission Control
+# # (i.e. use the old Exposé behavior instead)
+# defaults write com.apple.dock expose-group-by-app -bool false
+#
+# # Disable Dashboard
+# defaults write com.apple.dashboard mcx-disabled -bool true
+#
+# # Don’t show Dashboard as a Space
+# defaults write com.apple.dock dashboard-in-overlay -bool true
+#
+
+
+
+# Disable the Launchpad gesture (pinch with thumb and three fingers)
+#defaults write com.apple.dock showLaunchpadGestureEnabled -int 0
+
+# # Reset Launchpad, but keep the desktop wallpaper intact
+# find "${HOME}/Library/Application Support/Dock" -name "*-*.db" -maxdepth 1 -delete
+#
+# # Add iOS & Watch Simulator to Launchpad
+# sudo ln -sf "/Applications/Xcode.app/Contents/Developer/Applications/Simulator.app" "/Applications/Simulator.app"
+# sudo ln -sf "/Applications/Xcode.app/Contents/Developer/Applications/Simulator (Watch).app" "/Applications/Simulator (Watch).app"
+
+# Hot corners
+# Possible values:
+#  0: no-op
+#  2: Mission Control
+#  3: Show application windows
+#  4: Desktop
+#  5: Start screen saver
+#  6: Disable screen saver
+#  7: Dashboard
+# 10: Put display to sleep
+# 11: Launchpad
+# 12: Notification Center
+# Top left screen corner → Mission Control
+# defaults write com.apple.dock wvous-tl-corner -int 2
+# defaults write com.apple.dock wvous-tl-modifier -int 0
+# # Top right screen corner → Desktop
+# defaults write com.apple.dock wvous-tr-corner -int 4
+# defaults write com.apple.dock wvous-tr-modifier -int 0
+# # Bottom left screen corner → Start screen saver
+# defaults write com.apple.dock wvous-bl-corner -int 5
+# defaults write com.apple.dock wvous-bl-modifier -int 0
+
+
+
+
+killall Dock
