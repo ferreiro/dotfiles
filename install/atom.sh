b75e66cd4df1734c70b9d605d0446534767479a9
Jorge Ferreiro <me@jgferreiro.com>
2016-03-23 22:22:05 +0100

    Cleaned atom.sh using array of packages

--- install/atom.sh
+++ install/atom.sh
@@ -4,50 +4,46 @@
 # http://www.hongkiat.com/blog/useful-atom-packages/
 # https://atom.io/packages/
 
-########################################################
-#### Install Atom packages / modules and more stuff ####
-########################################################
-
 echo
 echo "*******************************************************"
 echo "*** Install Atom packages / modules and more stuff  ***"
 echo "*******************************************************"
 echo
 
-#############
-### TOOLS ###
-#############
-
-apm install pigments
-apm install open-recent
-apm install script # https://atom.io/packages/script
-apm install linter # Install Linter using atom package manager: https://atom.io/packages/linter
-apm install minimap # https://atom.io/packages/minimap
-apm install atom-beautify # https://atom.io/packages/atom-beautify
-apm install merge-conflicts # https://atom.io/packages/merge-conflicts
-apm install highlight-selected # https://atom.io/packages/highlight-selected
-apm install project-manager # https://atom.io/packages/project-manager
-apm install tabs-to-spaces # https://atom.io/packages/tabs-to-spaces
-apm install file-icons # https://atom.io/packages/file-icons
-apm install emmet
-
-###############
-### LINTERS ###
-###############
-# More linters here: https://atom.io/users/AtomLinter/packages
-
-# PYTHON
+# Setting requirements for Python Linter.
 # Credits: http://www.marinamele.com/install-and-configure-atom-editor-for-python
 pip install flake8
 pip install flake8-docstrings
-apm install linter-flake8
-apm install atom-django
 
-# RUBY
-apm install linter-ruby
+########################################################
+#### Install Atom packages / modules and more stuff ####
+########################################################
 
-# JAVASCRIPT
-apm install linter-jshint
 
-# CSS
-apm install linter-csslint
+atom_packages=(
+
+    ### TOOLS ###
+    pigments
+    open-recent
+    script # https://atom.io/packages/script
+    linter # Install Linter using atom package manager: https://atom.io/packages/linter
+    minimap # https://atom.io/packages/minimap
+    atom-beautify # https://atom.io/packages/atom-beautify
+    merge-conflicts # https://atom.io/packages/merge-conflicts
+    highlight-selected # https://atom.io/packages/highlight-selected
+    project-manager # https://atom.io/packages/project-manager
+    tabs-to-spaces # https://atom.io/packages/tabs-to-spaces
+    file-icons # https://atom.io/packages/file-icons
+    emmet
+
+    ### LINTERS ###
+    # More linters here: https://atom.io/users/AtomLinter/packages
+    # WARNING!!! Python linter needs flake8 $ pip install flake8 || $ pip install flake8-docstrings
+    linter-flake8
+    linter-ruby
+    linter-jshint # Javascript
+    linter-csslint
+    atom-django
+)
+
+apm install "${atom_packages[@]}"
