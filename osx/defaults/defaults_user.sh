fa1e8d7ff3d7952733cb761071e11d34785c83d7
Jorge Ferreiro <me@jgferreiro.com>
2016-03-23 04:38:25 +0100

    Added defaults user

---
+++ osx/defaults/defaults_user.sh
@@ -0,0 +1,11 @@
+COMPUTER_NAME="George"
+
+###############################################################################
+# General UI/UX                                                               #
+###############################################################################
+
+# Set computer name (as done via System Preferences → Sharing)
+sudo scutil --set ComputerName "$COMPUTER_NAME"
+sudo scutil --set HostName "$COMPUTER_NAME"
+sudo scutil --set LocalHostName "$COMPUTER_NAME"
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$COMPUTER_NAME"
