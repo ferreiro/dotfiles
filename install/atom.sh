26905e2ff8ada45c56341f6358b4ea89be3a6b67
Jorge Ferreiro <me@jgferreiro.com>
2016-03-23 22:58:59 +0100

    Added links to atom packages

--- install/atom.sh
+++ install/atom.sh
@@ -12,6 +12,7 @@ echo
 
 # Setting requirements for Python Linter.
 # Credits: http://www.marinamele.com/install-and-configure-atom-editor-for-python
+
 pip install flake8
 pip install flake8-docstrings
 
@@ -19,12 +20,12 @@ pip install flake8-docstrings
 #### Install Atom packages / modules and more stuff ####
 ########################################################
 
-
 atom_packages=(
 
     ### TOOLS ###
-    pigments
-    open-recent
+
+    pigments # https://atom.io/packages/pigments
+    open-recent # https://atom.io/packages/open-recent
     script # https://atom.io/packages/script
     linter # Install Linter using atom package manager: https://atom.io/packages/linter
     minimap # https://atom.io/packages/minimap
@@ -34,16 +35,18 @@ atom_packages=(
     project-manager # https://atom.io/packages/project-manager
     tabs-to-spaces # https://atom.io/packages/tabs-to-spaces
     file-icons # https://atom.io/packages/file-icons
-    emmet
+    emmet # https://atom.io/packages/emmet
+    todo-show # https://atom.io/packages/todo-show
 
     ### LINTERS ###
     # More linters here: https://atom.io/users/AtomLinter/packages
     # WARNING!!! Python linter needs flake8 $ pip install flake8 || $ pip install flake8-docstrings
-    linter-flake8
-    linter-ruby
-    linter-jshint # Javascript
-    linter-csslint
-    atom-django
+
+    linter-flake8 # https://atom.io/packages/linter-flake8
+    linter-ruby # https://atom.io/packages/linter-ruby
+    linter-jshint # https://atom.io/packages/linter-jshint
+    linter-csslint # https://atom.io/packages/linter-csslint
+    atom-django # https://atom.io/packages/atom-django
 )
 
 apm install "${atom_packages[@]}"
