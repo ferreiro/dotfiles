18d50b9087f442a32ed854698454fdf277a2ab8b
Jorge Ferreiro <me@jgferreiro.com>
2016-03-23 21:35:33 +0100

    Added more atom packages

--- install/atom.sh
+++ install/atom.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Some cool references places to find packages:
+# http://www.hongkiat.com/blog/useful-atom-packages/
+# https://atom.io/packages/
+
 ########################################################
 #### Install Atom packages / modules and more stuff ####
 ########################################################
@@ -15,12 +19,17 @@ echo
 #############
 
 apm install pigments
-apm install highlight-selected
 apm install open-recent
+apm install script # https://atom.io/packages/script
 apm install linter # Install Linter using atom package manager: https://atom.io/packages/linter
 apm install minimap # https://atom.io/packages/minimap
 apm install atom-beautify # https://atom.io/packages/atom-beautify
 apm install merge-conflicts # https://atom.io/packages/merge-conflicts
+apm install highlight-selected # https://atom.io/packages/highlight-selected
+apm install project-manager # https://atom.io/packages/project-manager
+apm install tabs-to-spaces # https://atom.io/packages/tabs-to-spaces
+apm install file-icons # https://atom.io/packages/file-icons
+apm install emmet
 
 ###############
 ### LINTERS ###
@@ -32,6 +41,7 @@ apm install merge-conflicts # https://atom.io/packages/merge-conflicts
 pip install flake8
 pip install flake8-docstrings
 apm install linter-flake8
+apm install atom-django
 
 # RUBY
 apm install linter-ruby
