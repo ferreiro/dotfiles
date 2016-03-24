11fb0b6e4549cd1b025a5996a0f595510d55f998
Jorge Ferreiro <me@jgferreiro.com>
2016-03-24 04:41:10 +0100

    Refactored defaults from system to app_store

---
+++ osx/defaults/defaults_app_store.sh
@@ -0,0 +1,9 @@
+# ###############################################################################
+# # Mac App Store                                                               #
+# ###############################################################################
+
+# Enable the WebKit Developer Tools in the Mac App Store
+defaults write com.apple.appstore WebKitDeveloperExtras -bool true
+
+# Enable Debug Menu in the Mac App Store
+defaults write com.apple.appstore ShowDebugMenu -bool true
--- osx/defaults/defaults_system.sh
+++ osx/defaults/defaults_system.sh
@@ -338,16 +338,7 @@ sudo nvram SystemAudioVolume=" "
 # defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
 # defaults write com.apple.ActivityMonitor SortDirection -int 0
 #
-# ###############################################################################
-# # Mac App Store                                                               #
-# ###############################################################################
-#
-# # Enable the WebKit Developer Tools in the Mac App Store
-# defaults write com.apple.appstore WebKitDeveloperExtras -bool true
-#
-# # Enable Debug Menu in the Mac App Store
-# defaults write com.apple.appstore ShowDebugMenu -bool true
-#
+
 # ###############################################################################
 # # SSD-specific tweaks                                                         #
 # ###############################################################################
