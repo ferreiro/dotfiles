735fe732de25abdcd476cb47c90f461f9745525a
Jorge Ferreiro <me@jgferreiro.com>
2016-03-24 02:02:11 +0100

    Deleted all formulas and mac programs on uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -26,6 +26,10 @@ fi
 ### Remove Symbolic links to configuration files ###
 ####################################################
 
+echo
+echo "=> Unmount all symbolic links"
+echo
+
 unmount_symbolic_links () {
   unlink ~/.bash_profile
   unlink ~/.inputrc
@@ -33,10 +37,69 @@ unmount_symbolic_links () {
   unlink ~/.gitignore_global
   unlink ~/.gemrc
   unlink ~/.atom
+  echo "   [UNMOUNTED] All symbolic links were deleted"
 }
 
 unmount_symbolic_links # call function
 
-# TODO: Uninstall all the programs inside "install/" folder.
+# Restore all mac defaults
+
+echo
+echo "=> Erase Mac OSX Defaults:"
+echo
+
+# defaults_list=(
+#   Dock
+#   Finder
+#   #TODO: You need to include more defaults here. or find a way to delete all at onces
+#   #"Address Book"
+#   #Calendar
+#   #Contacts
+#   #Mail
+# )
+# defaults delete com.apple."${defaults_list[@]}";
+
+echo "   [DELETED] All defaults were deleted"
+
+
+### Uninstall brew formulas ###
+
+echo
+echo "=> Erase Homebrew formulas:"
+echo
+
+total_formulas="$(brew list)"
+
+if [ ! -n "$total_formulas" ]
+then
+  echo "   [EMPTY] The are not any brew formulas to be erased"
+else
+  echo "   [YES] The are some brew formulas installed"
+  echo "         Erasing brew formulas"
+  echo
+  brew list | xargs brew uninstall --force
+fi
+
+### Uninstall brew formulas ###
+
+echo
+echo "=> Erase Homebrew cask applications:"
+echo
+
+total_cask_apps="$(brew cask list)"
+
+if [ ! -n "$total_cask_apps" ]
+then
+  echo "   [EMPTY] The are not any brew cask applications to be erased"
+else
+  echo "   [YES] The are some brew cask applications"
+  echo "         Erasing brew cask applications"
+
+  brew cask list | xargs brew cask uninstall --force
+fi
+
+
+### Kill all to apply changes ###
+killall Dock
 
 exit 1
